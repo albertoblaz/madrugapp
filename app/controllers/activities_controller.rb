6bef2fd00244e64d24c785bc6d19d1d6b3060fae
Alberto Blazquez <alberto.blazquezrodriguez@shopify.com>
2022-07-22 01:15:32 +0200

    Create new activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -3,4 +3,20 @@ class ActivitiesController < ApplicationController
     @project = Project.find(params[:project_id])
     @activities = @project.activities
   end
+
+  def new
+    @project = Project.find(params[:project_id])
+  end
+
+  def create
+    @project = Project.find(params[:project_id])
+    @activity = @project.activities.create(create_activity_params)
+    redirect_to project_activities_path
+  end
+
+  private
+
+  def create_activity_params
+    params.require(:activity).permit(:name, :description, :ui_color)
+  end
 end
