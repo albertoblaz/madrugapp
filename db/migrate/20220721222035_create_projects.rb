8621c52b0b35ea44afbcbaf6dc28d0bebfba88fa
Alberto Blazquez <alberto.blazquezrodriguez@shopify.com>
2022-07-22 21:36:39 +0200

    Refactor Project: declare name not nullable

--- db/migrate/20220721222035_create_projects.rb
+++ db/migrate/20220721222035_create_projects.rb
@@ -1,7 +1,7 @@
 class CreateProjects < ActiveRecord::Migration[7.0]
   def change
     create_table :projects do |t|
-      t.string :name
+      t.string :name, null: false
       t.string :description
       t.string :image_url
       t.date :start_date
--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_21_234554) do
   end
 
   create_table "projects", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.string "description"
     t.string "image_url"
     t.date "start_date"
